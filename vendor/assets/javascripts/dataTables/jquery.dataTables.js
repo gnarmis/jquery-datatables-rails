189b386df490cc1ac08720fe77968491bf495127
jeperkins4 <jeperkins4@gmail.com>
2013-10-10 16:58:55 -0400

    Added class to div wrapper

--- vendor/assets/javascripts/dataTables/jquery.dataTables.js
+++ vendor/assets/javascripts/dataTables/jquery.dataTables.js
@@ -2075,7 +2075,7 @@
 
 			var sSearchStr = oSettings.oLanguage.sSearch;
 			sSearchStr = (sSearchStr.indexOf('_INPUT_') !== -1) ?
-			  sSearchStr.replace('_INPUT_', '<input type="text" />') :
+			  sSearchStr.replace('_INPUT_', '<input type="text" class="form-control"/>') :
 			  sSearchStr==="" ? '<input type="text" />' : sSearchStr+' <input type="text" />';
 
 			var nFilter = document.createElement( 'div' );
